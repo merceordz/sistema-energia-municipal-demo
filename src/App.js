4a33cfffb121a214010f5c5c509b2fa55e6f64ae
Mer <merce.ordz@gmail.com>
2025-08-26 14:20:02 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MunicipalEnergyApp from './MunicipalEnergyApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MunicipalEnergyApp />
+    </div>
+  );
+}
+
+export default App;
